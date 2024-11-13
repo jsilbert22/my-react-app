d1c12be222b4279efe294ff23973c8b5c5259755
Jacob Silbert <j.d.silbert@wustl.edu>
2024-11-13 16:27:43 -0600

    redefined board component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
